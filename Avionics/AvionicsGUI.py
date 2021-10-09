4a3e3b9f00d02b3a98825713c6f3c7648226981a
Derick Bui <derickbui64@gmail.com>
2021-10-08 20:32:16 -0700

    Update AvionicsGUI.py

--- Avionics/AvionicsGUI.py
+++ Avionics/AvionicsGUI.py
@@ -1,48 +1,16 @@
-# Import tkinter library, only reason to import it
-# like this is just to mark down tk methods
-import tkinter as tk
-
-# All button methods------------------------------------------------------------------------------------
-def ActivateButton(number):
-    global buttonStringList
-    if (buttonStringList[number].get() == "Off"):
-        buttonStringList[number].set("On")
-        blankFunction(number)
-    else:
-        buttonStringList[number].set("Off")
-        blankFunction(number)
-
-def DeactivateButton(number):
-    pass
-
-def blankFunction(number):
-    pass
-
-def SwitchMode(number): # Function that switch mode
-    global fireModeOn
-    global keyNum
-    global stringMode
-    if (number == 0): # Turns on Test Mode
-        fireModeOn = False
-        keyNum = 0 # Reset key num
-        stringMode.set("Test Mode")
-        print("Test Mode On")
-    elif (number == 1): # Turns on Fire Mode
-        if (keyNum < 2): # Number Key increments
-            keyNum += 1
-            stringMode.set("Press " + str(3 - keyNum) + " more times to turn on Fire Mode")
-            print("Key incremented")
-        elif (keyNum == 2): # Fire mode turns on after number key is pressed three times
-            fireModeOn = True
-            stringMode.set("Fire Mode")
-            print("Fire Mode On")
-    check()
-
-def check():
-    print(fireModeOn)
+import tkinter as tk # Import tkinter GUI library
+import random as rand
+import serial # Import serial library for python connect with arduino (do pip install pyserial on consolepip)
+import can # Import CAN library (do python pip install python-can)
 
 # Root for application------------------------------------------------------------------------------------
 root = tk.Tk()
+
+# Functions-----------------------------------------------------------------------------------------------
+# def randomNumber(): # for test, will be trashed
+#     global upperPropSystemLabels
+#     upperPropSystemLabels[2]["text"] = "Temp: " + str(rand.randint(0,100))
+#     upperPropSystemLabels[2].after(1, randomNumber)
 
 # Create size for GUI window------------------------------------------------------------------------------
 # Window size presets
@@ -52,74 +20,116 @@ WIDTH = 1920
 canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
 canvas.pack()
 
-# Frame to have all control buttons-----------------------------------------------------------------------
-buttonFrame = tk.Frame(root, bg="Blue")
-buttonFrame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.2)
+#Group 1 Mode Display-------------------------------------------------------------------------------------
+#Create group 1 frame
+group1ModeFrame = tk.Frame(root, bg="Aqua", bd=5)
+group1ModeFrame.place(relx=0, rely=0, relwidth=2/3, relheight=1)
 
-# Buttons
-button1 = tk.Button(buttonFrame, text="Button 1", command=lambda: ActivateButton(0))
-button1.place(relx=0, rely=0, relwidth=0.1, relheight=0.166)
-button2 = tk.Button(buttonFrame, text="Button 2", command=lambda: ActivateButton(1))
-button2.place(relx=0, rely=0.1666, relwidth=0.1, relheight=0.166)
-button3 = tk.Button(buttonFrame, text="Button 3", command=lambda: ActivateButton(2))
-button3.place(relx=0, rely=0.3332, relwidth=0.1, relheight=0.166)
-button4 = tk.Button(buttonFrame, text="Button 4", command=lambda: ActivateButton(3))
-button4.place(relx=0, rely=0.4998, relwidth=0.1, relheight=0.166)
-button5 = tk.Button(buttonFrame, text="Button 5", command=lambda: ActivateButton(4))
-button5.place(relx=0, rely=0.6664, relwidth=0.1, relheight=0.166)
-button6 = tk.Button(buttonFrame, text="Button 6", command=lambda: ActivateButton(5))
-button6.place(relx=0, rely=0.8330, relwidth=0.1, relheight=0.166)
+# Create top label frame and three smaller frames for nodes-----------------------------
+topLabelFrame = tk.Frame(group1ModeFrame, bg="black", bd=5)
+topLabelFrame.place(relx=0.5, rely=0, relwidth=0.9, relheight=0.2, anchor="n")
 
-# Button String Variables
-buttonStringList = [0] * 6
+# Number of nodes needed
+nodes = 3
 
-for i in range(len(buttonStringList)):
-    buttonStringList[i] = tk.StringVar()
-    buttonStringList[i].set("Off")
-
-buttonLabel1 = tk.Label(buttonFrame, textvariable=buttonStringList[0], bg="white", borderwidth=2, relief="solid", anchor="center")
-buttonLabel1.place(relx=0.1, rely=0, relwidth=0.1, relheight=0.166)
-buttonLabel2 = tk.Label(buttonFrame, textvariable=buttonStringList[1], bg="white", borderwidth=2, relief="solid", anchor="center")
-buttonLabel2.place(relx=0.1, rely=0.1666, relwidth=0.1, relheight=0.166)
-buttonLabel3 = tk.Label(buttonFrame, textvariable=buttonStringList[2], bg="white", borderwidth=2, relief="solid", anchor="center")
-buttonLabel3.place(relx=0.1, rely=0.3332, relwidth=0.1, relheight=0.166)
-buttonLabel4 = tk.Label(buttonFrame, textvariable=buttonStringList[3], bg="white", borderwidth=2, relief="solid", anchor="center")
-buttonLabel4.place(relx=0.1, rely=0.4998, relwidth=0.1, relheight=0.166)
-buttonLabel5 = tk.Label(buttonFrame, textvariable=buttonStringList[4], bg="white", borderwidth=2, relief="solid", anchor="center")
-buttonLabel5.place(relx=0.1, rely=0.6664, relwidth=0.1, relheight=0.166)
-buttonLabel6 = tk.Label(buttonFrame, textvariable=buttonStringList[5], bg="white", borderwidth=2, relief="solid", anchor="center")
-buttonLabel6.place(relx=0.1, rely=0.8330, relwidth=0.1, relheight=0.166)
+# nodeFrames[0]: Prop System Node Frame
+# nodeFrames[1]: Engine Node Frame
+# nodeFrames[2]: Pad Group Node Frame
+nodeFrames = [0] * nodes
+for i in range(nodes):
+    nodeFrames[i] = tk.Frame(topLabelFrame, bg="white", bd=5)
+    nodeFrames[i].place(relx=((1/nodes) + 0.005)*i, rely=0, relwidth=(1/(nodes + 0.1)), relheight=1)
 
 
-# Frame to contain the current mode shown on GUI-----------------------------------------------------------
-# Mode safety variables
-fireModeOn = False # Boolean for mode (False means Test Mode / True means Fire Mode)
-keyNum = 0 # Number of times fire mode button pressed will activate fire mode
-stringMode = tk.StringVar() # String for display current mode
-stringMode.set("Test Mode") # Set string
+# Upper Prop System node labels------------------------------------
+# upperPropSystemLabels[0]: Node Name (Upper Prop System Node)
+# upperPropSystemLabels[1]: Upper Prop System Activity
+# upperPropSystemLabels[2]: Uppoer Prop System Temperature
+# upperPropSystemLabels[3]: Sampling / Bus Info
+upperPropSystemLabels = [0] * 4
+for i in range(4):
+    upperPropSystemLabels[i] = tk.Label(nodeFrames[0], text="", bg="blue", anchor="w")
+    upperPropSystemLabels[i].place(relx=0, rely=(1/4)*i, relwidth=2/3, relheight=1/4)
 
-modeDisplayFrame = tk.Frame(root, bg="green", bd = 5)
-modeDisplayFrame.place(relx=0.35, rely=0.75, relwidth=0.3, relheight=0.05)
+upperPropSystemLabels[0]["text"] = "Upper Prop System Node"
+upperPropSystemLabels[1]["text"] = "Activity: "
+upperPropSystemLabels[2]["text"] = "Temp: "
+upperPropSystemLabels[3]["text"] = "Sampling / Bus Info"
 
-modeLabel = tk.Label(modeDisplayFrame, textvariable=stringMode, bg="white", borderwidth=2, relief="solid", anchor="center")
-modeLabel.place(relx=0, rely=0, relwidth=1, relheight=1)
+# Engine node labels------------------------------------
+# engineLabels[0]: Node Name (Engine Node)
+# engineLabels[1]: Engine Activity
+# engineLabels[2]: Engine Temperature
+# engineLabels[3]: Sampling / Bus Info
+engineLabels = [0] * 4
+for i in range(4):
+    engineLabels[i] = tk.Label(nodeFrames[1], text="", bg="blue", anchor="w")
+    engineLabels[i].place(relx=0, rely=(1/4)*i, relwidth=2/3, relheight=1/4)
 
-# Frame to contain test and actual fire button-------------------------------------------------------------
-testFrame = tk.Frame(root, bg="green", bd=5)
-testFrame.place(relx=0.35, rely=0.8, relwidth=0.3, relheight=0.1)
+engineLabels[0]["text"] = "Upper Prop System Node"
+engineLabels[1]["text"] = "Activity: "
+engineLabels[2]["text"] = "Temp: "
+engineLabels[3]["text"] = "Sampling / Bus Info"
 
-testModeButton = tk.Button(testFrame, text="Test Mode", command=lambda: SwitchMode(0))
-testModeButton.place(relx=0, rely=0, relwidth=0.5, relheight=1)
+# Pad Ground node labels------------------------------------
+# padGroundLabels[0]: Node Name (Pad Ground Node)
+# padGroundLabels[1]: Pad Ground Activity
+# padGroundLabels[2]: Pad Ground Temperature
+# padGroundLabels[3]: Sampling / Bus Info
+padGroundLabels = [0] * 4
+for i in range(4):
+    padGroundLabels[i] = tk.Label(nodeFrames[2], text="", bg="blue", anchor="w")
+    padGroundLabels[i].place(relx=0, rely=(1/4)*i, relwidth=2/3, relheight=1/4)
 
-fireModeButton = tk.Button(testFrame, text="Fire Mode", command=lambda: SwitchMode(1))
-fireModeButton.place(relx=0.5, rely=0, relwidth=0.5, relheight=1)
+padGroundLabels[0]["text"] = "Pad Ground Node"
+padGroundLabels[1]["text"] = "Activity: "
+padGroundLabels[2]["text"] = "Temp: "
+padGroundLabels[3]["text"] = "Sampling / Bus Info"
 
-# To type in input
-# entry = tk.Entry(frame, bg="white")
-# entry.pack()
+# nodeFrameStateLabels[0]: Prop System Node State Label
+# nodeFrameStateLabels[1]: Engine Node State Label
+# nodeFrameStateLabels[2]: Pad Group Node State Label
+nodeFrameStateLabels = [0] * nodes
+for i in range(nodes):
+    nodeFrameStateLabels[i] = tk.Label(nodeFrames[i], text="", bg="aqua")
+    nodeFrameStateLabels[i].place(relx=2/3, rely=2/3, relwidth=(1/3), relheight=1/3)
 
-# Initialize settings
-SwitchMode(0)
+# Create 12 Button Frame---------------------------------------
+g1RowFrame1 = tk.Frame(group1ModeFrame, bg="Red", bd=5)
+g1RowFrame1.place(relx=0.5, rely=0.2, relwidth=0.9, relheight=0.2, anchor="n")
 
-# Start window
+# List to store buttons for g1RowFrame1
+g1ButtonRowList1 = [0]*12
+
+# Create first row of buttons
+for i in range(12): # Create buttons for g1ForFrame1
+    if (i < 6):
+        g1ButtonRowList1[i] = tk.Button(g1RowFrame1, text="G1 F1 Button " + str(i + 1))
+        g1ButtonRowList1[i].place(relx=((1/6) * i), rely=0, relwidth=(1/6), relheight=0.5)
+    else:
+        g1ButtonRowList1[i] = tk.Button(g1RowFrame1, text="G1 F1 Button " + str(i + 1))
+        g1ButtonRowList1[i].place(relx=((1/6) * (i - 6)), rely=0.5, relwidth=(1/6), relheight=0.5)
+
+
+# Create 6 Button Frame
+g1RowFrame2 = tk.Frame(group1ModeFrame, bg="Red", bd=5)
+g1RowFrame2.place(relx=0.5, rely=0.5, relwidth=0.9, relheight=0.1, anchor="n")
+
+# List to store buttons for g1RowFrame2
+g1ButtonRowList2 = [0]*6
+
+# Create row of buttons
+for i in range(6): # Create buttons for g1ForFrame1
+    g1ButtonRowList2[i] = tk.Button(g1RowFrame2, text="G1 F2 Button " + str(i + 1))
+    g1ButtonRowList2[i].place(relx=((1/6) * i), rely=0, relwidth=(1/6), relheight=1)
+
+#Group 2 Mode Display--------------------------------------------------------------------------------------
+#Create group 2 frame
+group2ModeFrame = tk.Frame(root, bg="Blue", bd=5)
+group2ModeFrame.place(relx=2/3, rely=0, relwidth=1/3, relheight=1)
+
+# Realtime stuff
+# root.after(1, randomNumber)
+
+# Start window--------------------------------------------------------------------------------------------
 root.mainloop()
